a1bc771a865f324cf7448ff557494adf1d7a8015
Adriel <adriel.fsantos@outlook.com>
2022-05-16 20:34:28 -0300

    Adiciona resolucao das questoes 3 e 4

--- Texto03.java
+++ Texto03.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.io.File;
 
 /**
  * Resolução da Lista 2 da unidade 1
@@ -32,5 +33,45 @@ public class Texto03
             pe.printStackTrace();
         }
         return formatadorString.format(novaData);
+    }
+    
+    /*
+     * Receber um caminho de diretório como parâmetro e retornar um vetor
+     * com todos os itens do diretório. Dica: objetos da classe File podem
+     * ser usados para realizar operações sobre diretórios, entre várias
+     * outras coisas. Analise a documentação da classe File para saber além
+     * do que já foi descrito no conteúdo deste texto.
+     */
+    public File[] exercicio3_03(String caminho) {
+        File pasta = new File(caminho);
+        return pasta.listFiles();
+    }
+    
+    /*
+     * Receber um caminho de diretório como parâmetro e retornar um vetor
+     * com todos os itens do diretório. Cada item do vetor retornado deve
+     * estar precedido por "[A]", caso seja arquivo, e "D", caso seja
+     * diretório. Retorne um valor nulo caso o String recebido como
+     * parâmetro não corresponda a um diretório existente no disco. Mais
+     * uma vez, você precisará de um objeto da classe File para fazer o
+     * trabalho sujo para você.
+     */
+    public String[] exercicio3_04(String caminho) {
+        File arquivos[] = exercicio3_03(caminho);
+        
+        if (arquivos == null) return null;
+        
+        String nomesArquivos[] = new String[arquivos.length];
+        
+        for (int i=0; i<arquivos.length; i++) {
+            if (arquivos[i].isFile()) {
+                nomesArquivos[i] = "[A] " + arquivos[i].getName();
+            } else if (arquivos[i].isDirectory()) {
+                nomesArquivos[i] = "D " + arquivos[i].getName();
+            } else {
+                nomesArquivos[i] = arquivos[i].getName();
+            }
+        }
+        return nomesArquivos;
     }
 }
